a88b81c61cf1f674c7b063a4cd9911159e7589a5
migarcos <migark@hotmail.com>
2026-08-18 16:55:52 -0500

    btn component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import MyButton from './components/MyButton'
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      < MyButton />
     </>
   )
 }
