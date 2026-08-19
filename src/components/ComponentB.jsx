d50ddbcf15bfb77dc8e1958e3d4057469dd3f8f9
migarcos <migark@hotmail.com>
2026-08-19 15:24:36 -0500

    Uncontroled Component B

--- src/components/ComponentB.jsx
+++ src/components/ComponentB.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const ComponentB = ({ textBtn }) => {
-  return (
-    <button> { textBtn } </button>
-  )
+
+    const ctrlClick = () => {
+        alert("Click was doing")
+    }
+    return (
+        <button onClick={ctrlClick}> { textBtn } </button>
+    )
 };
 
 export default ComponentB
